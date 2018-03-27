0e9376a73bba8f07337dc0def58db026a0f86865
Jeremias Pak <jeremiaspak@gmail.com>
2018-03-27 00:38:07 -0300

    Auto-generated commit

--- src/app/@theme/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './button/button.component';
-export * from './header/header.component';
-export * from './list/list.component';
-export * from './search-bar/search-bar.component';
-export * from './tabs/tabs.component';
